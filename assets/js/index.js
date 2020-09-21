a4bb3861a42f16f4dbd6c4feb57c4fd9f50a434a
Ilmira <tregulovailmira@gmail.com>
2020-09-21 17:16:38 +0300

    Refactor task on fetch, fix logNumbersWithTimeout()

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,27 @@
 'use strict';
 
+fetch('../../user.json')
+    .then((res) => res.json())
+    .then((users) => {
+        const list = document.createElement("ul");
+        const [body] = document.getElementsByTagName('body');
+        body.append(list);
+        const listItems = users.map((user) => {
+            const userInfo = [];
+            for(const [key, value] of Object.entries(user)){
+                userInfo.push(` ${key}: ${value}`);
+            }
+
+            const li = document.createElement('li');
+            li.textContent = userInfo.toString();
+            return li;
+        });
+
+        list.append(...listItems);
+    })
+    .catch(console.error);
+
+
 console.time('timeout');
 logNumbersWithTimeout();
 
@@ -13,7 +35,7 @@ function logNumbersWithTimeout() {
         i++;
         timeoutId = setTimeout(logNumbers, 100);
         if (i > 20) {
-            clearInterval(timeoutId);
+            clearTimeout(timeoutId);
             console.timeEnd('timeout');
         }
     }, 100);
